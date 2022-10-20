fea4eae011b884e107f2c926e3ad78f064d99e96
Sahil kalirawna <sahilkalirawna@gmail.com>
2022-10-20 16:59:27 +0530

    example in App jg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { FlipCard, MyAccordion } from "component-library-react-12";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<FlipCard />
+			<MyAccordion
+				label='Test name'
+				description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolores repudiandae quas.'
+        color="red"
+			/>
+		</div>
+	);
 }
 
 export default App;
